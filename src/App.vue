bc3216653ed6215f7842e95e28e33c4c8e78dbea
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-24 12:55:25 +0200

    first step with vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
-<script setup>
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
 </script>
 
 <template>
-    <h1>First App in Vue</h1>
+    <h1>sierant.me</h1>
+    <div>
+        <button @click="count++">Plus</button>
+        <h3>{{ count }}</h3>
+        <button @click="count--">Minus</button>
+    </div>
 </template>
+
+<style scoped>
+div {
+    display: flex;
+    gap: 1rem;
+}
+</style>
