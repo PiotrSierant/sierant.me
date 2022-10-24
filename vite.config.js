be73d8c401a70a66e0c6e01933343bd9cab00531
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-24 19:29:19 +0200

    vite cfg

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sierant.me/',
+  base: '/vue/',
   plugins: [vue()],
   resolve: {
     alias: {
