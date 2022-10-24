a4d92197c2e53e184a90852d8db79dc88d7e79cc
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-24 14:34:08 +0200

    studying vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@ export default {
         return {
             count: 0
         }
+    },
+    methods: {
+        increment() {
+            this.count += 1;
+        },
+        decrement() {
+            this.count -= 1;
+        },
+        cleanCount() {
+            this.count = 0;
+        }
     }
 }
 </script>
@@ -11,14 +22,29 @@ export default {
 <template>
     <h1>sierant.me</h1>
     <div>
-        <button @click="count++">Plus</button>
         <h3>{{ count }}</h3>
-        <button @click="count--">Minus</button>
+        <section class="button__section">
+            <button @click="increment">Plus</button>
+            <button @click="decrement">Minus</button>
+            <button @click="cleanCount">Zeruj</button>
+        </section>
     </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    h3 {
+        font-size: 40px;
+    }
+}
+
+.button__section {
     display: flex;
     gap: 1rem;
 }
