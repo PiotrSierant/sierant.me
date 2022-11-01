94d45769cd15757d1535ac1bcfc90d9321dfc9d5
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-01 10:20:23 +0100

    watch func

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -6,12 +6,16 @@
         <strong>{{ sharesValue }}$</strong>, because share price is
         <strong>{{sharePrice}}$</strong>.
       </p>
+      <button @click="changeNumberOfShares(1)">Buy one share</button>
+      <button @click="changeNumberOfShares(5)">Buy five share</button>
+      <button @click="changeNumberOfShares(-1)">Sell one share</button>
+      <button @click="changeNumberOfShares(-5)">Sell five share</button>
     </div>
   </section>
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 
 export default {
   name: "Watch",
@@ -19,7 +23,26 @@ export default {
     const shares = ref(15);
     const sharePrice = ref(20);
     const sharesValue = computed(() => shares.value * sharePrice.value);
-    return { shares, sharePrice, sharesValue };
+
+    function changeNumberOfShares(number) {
+      if(shares.value + number >=0) {
+        shares.value += number;
+      }
+    }
+
+    watch(shares, (shares, prevShares) => {
+      console.log('shares: ', shares, 'prevShares: ', prevShares);
+      shares > prevShares ? getPrice(1,5) : getPrice(-5,-1);
+    })
+
+    function getPrice(min, max) {
+      const priceDiff = Math.floor(Math.random() * (max - min) + min)
+      if(sharePrice.value + priceDiff >= 0) {
+        sharePrice.value += priceDiff;
+      }
+    }
+
+    return { shares, sharePrice, sharesValue, changeNumberOfShares };
   }
 };
 </script>
@@ -30,10 +53,18 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
     div {
       max-width: 1366px;
       padding: 1rem 0;
     }
   }
-
+  button {
+    margin-top: .5rem;
+    padding: 1rem 2rem;
+    border: 1px solid #42b883;
+    background-color: #35495e;
+    color: whitesmoke;
+    cursor: pointer;
+  }
 </style>
